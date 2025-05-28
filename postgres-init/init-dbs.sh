a0bb33c505524a3dde07b83c236247340639bcde
unite-abilities <71151282+unite-abilities@users.noreply.github.com>
2025-05-28 10:04:33 +1000

    Update init-dbs.sh

--- postgres-init/init-dbs.sh
+++ postgres-init/init-dbs.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -e # Exit immediately if a command exits with a non-zero status.
 
-# This script is run when the PostgreSQL container starts for the first time.
-# It creates an additional database for OpenWebUI.
-# The main database (defined by POSTGRES_DB in .env) is created automatically by the postgres image.
+echo "Checking for and creating database: $OPENWEBUI_DB_NAME"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    SELECT 'CREATE DATABASE ${OPENWEBUI_DB_NAME}'
-    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${OPENWEBUI_DB_NAME}')\gexec
+    DO
+    $do$
+    BEGIN
+        IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '${OPENWEBUI_DB_NAME}') THEN
+            CREATE DATABASE "${OPENWEBUI_DB_NAME}";
+        END IF;
+    END
+    $do$;
 EOSQL
 
 echo "PostgreSQL: Checked/Created database '$OPENWEBUI_DB_NAME' for OpenWebUI."
